d0048496e8689207d809e5a407818af76d274237
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-03-11 12:55:16 +0700

    feat(depedency): learn lerna link

--- _base/helper/lib/helper.js
+++ _base/helper/lib/helper.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const model = require('@irfanandriansyah1997/model/lib/model');
+
 module.exports = helper;
 
 function helper() {
+    model();
     console.debug('sample helper last version')
 }
+
+helper()
